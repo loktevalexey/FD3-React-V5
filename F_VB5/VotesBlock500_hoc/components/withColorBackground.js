1a97cdf272fad2dc0044c6fddfcd375a37978181
loktev <loktev@tut.by>
2019-03-09 08:10:31 +0300

    RF500

---
+++ F_VB5/VotesBlock500_hoc/components/withColorBackground.js
@@ -0,0 +1,13 @@
+﻿import React from 'react';
+
+function withColorBackground(Component) {
+    // HOC withColorBackground должен вернуть КОМПОНЕНТ
+    // в данном случае возвращаем компонент в функциональном стиле
+    return props => (
+      <div style={{backgroundColor:"yellow"}}>
+        <Component {...props} />
+      </div>
+    );
+}
+
+export { withColorBackground };
